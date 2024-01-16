e32a0a5ffca2312fd1ece107fc17c53293725059
Anderson César <anderson.cbcl3@gmail.com>
2024-01-16 16:55:34 -0300

    Alteração na data class usuario: parametros e funções; novos TODOs.

--- desafio.kt
+++ desafio.kt
@@ -1,15 +1,37 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+TODO("")
 
-TODO("Adicionar parâmetros para os usuários.")
-data class Usuario
 
-TODO("Alterar a duração de acordo com os conteúdos")
+
+//TODO("Adicionar parâmetros para os usuários.")
+TODO("Entender se é para ser val ou var")
+TODO("Testar o gerador de id")
+data class Usuario(
+	val nome: String, val email: String, val id: Int
+){      
+    override fun equals(other: Any?) =
+        other is Usuario && other.id == this.id 
+    fun geradorDeId(id: Int){
+        val id = Usuario.hashCode()
+    }
+}
+
+
+
+//TODO("Alterar a duração de acordo com os conteúdos duracao -> var")
 TODO("adicionar uma lista de conteudoEducacional")
-data class ConteudoEducacional(val nome: String, var duracao: Int = 60)
+data class ConteudoEducacional(
+    val nome: String, var duracao: Int = 60
+)
 
-data class Formacao(val nome: String, /*var nivel*/, var conteudos: List<ConteudoEducacional>) {
+
+
+
+data class Formacao(
+    val nome: String, val nivel: String, var conteudos: List<ConteudoEducacional>
+) {
 	
     TODO("Passar os usuários para a lista de inscritos após serem matriculados.")
     val inscritos = mutableListOf<Usuario>()
@@ -19,10 +41,14 @@ data class Formacao(val nome: String, /*var nivel*/, var conteudos: List<Conteud
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
-TODO("")
+
+
+
+
 fun main() {
+    val aluno1 = Usuario("Anderson", "anderson.cbcl3@gmail.com")
+    
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 	TODO("Imprimir lista de matriculados em suas formações")
 }
-
